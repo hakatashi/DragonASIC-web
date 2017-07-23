8a4b51a6c7a86ba93487e30f3410078d21b6a940
Travis CI <info@tsg.ne.jp>
2017-07-23 01:21:00 +0000

    Update build - 0f0fe98711e85571c9ad0062c628df28c6c65632

--- src/Wire.jsx
+++ src/Wire.jsx
@@ -7,19 +7,43 @@ const styles = require('./Wire.pcss');
 class Wire extends React.Component {
 	constructor(props, state) {
 		super(props, state);
+
+		this.state = {
+			startX: this.props.mode === 'relative'
+				? 0
+				: this.props.start.dimension.left + this.props.start.dimension.width / 2,
+			startY: this.props.mode === 'relative'
+				? 0
+				: this.props.start.dimension.top + this.props.start.dimension.height / 2,
+			endX: this.props.mode === 'relative'
+				? this.props.x
+				: this.props.end.dimension.left + this.props.start.dimension.width / 2,
+			endY: this.props.mode === 'relative'
+				? this.props.y
+				: this.props.end.dimension.top + this.props.start.dimension.height / 2,
+		};
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (this.props.x !== nextProps.x || this.props.y !== nextProps.y) {
+			this.setState({
+				endX: nextProps.x,
+				endY: nextProps.y,
+			});
+		}
 	}
 
 	getPath = () => Path()
-		.moveTo(0, 0)
+		.moveTo(this.state.startX, this.state.startY)
 		.relative().curveTo(
-			this.props.x / 2, 0,
-			this.props.x / 2, this.props.y,
-			this.props.x, this.props.y)
+			(this.state.endX - this.state.startX) / 2, 0,
+			(this.state.endX - this.state.startX) / 2, this.state.endY - this.state.startY,
+			this.state.endX - this.state.startX, this.state.endY - this.state.startY)
 		.end();
 
 	render() {
 		return (
-			<svg styleName={classNames('wire', this.props.direction)}>
+			<svg styleName={classNames('wire', this.props.mode, this.props.direction)}>
 				<path
 					styleName="path"
 					d={this.getPath()}
